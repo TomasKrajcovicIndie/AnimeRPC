2b94f8aa9cb68c6ad65c25e35230992e3db48ae6
Tomáš Krajčovič <tomaskrajcovic@icloud.com>
2022-12-13 20:01:00 +0100

    Added requirements, Added next episode watched

--- AnimeRPC.py
+++ AnimeRPC.py
@@ -5,7 +5,6 @@ import requests
 import keyboard
 
 
-
 anilist = Anilist()
 client_id = '1045803809848430624'
 RPC = Presence(client_id)
@@ -28,7 +27,7 @@ $$/   $$/ $$/   $$/ $$/ $$/  $$/  $$/  $$$$$$$/ $$/   $$/ $$/        $$$$$$/
 
 def check_update():
     # check if there's a higher version of the app
-    commit_count = 3
+    commit_count = 5
     repo_commit_count = len(requests.get(
         "https://api.github.com/repos/TomasKrajcovicIndie/AnimeRPC/commits?per_page=100").json())
     if commit_count != repo_commit_count:
@@ -53,8 +52,13 @@ start = int(time.time())
 #print(RPC.update(large_image="logo", large_text=f"Created by Fostrullik#7274", state=f"Episode: {episode}", details=f"Anime: {name}", buttons=[{"label": "What am I watching?", "url": f"https://anilist.co/anime/{anime_dict}"}]))
 
 print('AnimeRPC running! Press CTRL+C or "q" to stop it.')
+print('Press "+" to change episode number by 1 after you watched episode.')
 while True:
     k = RPC.update(large_image="logo", large_text=f"Created by Fostrullik#7274", state=f"Episode: {episode}", details=f"Anime: {name}", start= start, buttons=[{"label": "What am I watching?", "url": f"https://anilist.co/anime/{anime_dict}"}])
     if keyboard.is_pressed('q'):
         print("Stopping AnimeRPC on user request...")
         break
+    elif keyboard.is_pressed("+"):
+        episode = int(episode) + 1
+        print(f"Episode changed to {episode}")
+        time.sleep(1)
